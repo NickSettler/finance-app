086c649c94ce471600128fc4bd5bf81dad21b8e3
NickSettler <moiseevnikita14@gmail.com>
2023-03-17 23:38:29 +0100

    Initial Commit

---
+++ Finance App/Finance_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finance_AppApp.swift
+//  Finance App
+//
+//  Created by Никита Моисеев on 17.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct Finance_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
