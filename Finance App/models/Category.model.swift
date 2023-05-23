71df3c040fc1668782c161eca6c2d4ab2c8cf9a0
NickSettler <moiseevnikita14@gmail.com>
2023-05-24 01:36:29 +0200

    refactor(models): update category model

--- Finance App/models/Category.model.swift
+++ Finance App/models/Category.model.swift
@@ -13,14 +13,22 @@ import FirebaseFirestoreSwift
 struct Category : FirebaseIdentifiable {
     @DocumentID var id: String?
     var name: String
+    var icon: String
     
     enum CodingKeys: CodingKey {
         case id
         case name
+        case icon
     }
     
-    init(id: String, name: String) {
+    init(name: String, icon: String) {
+        self.name = name
+        self.icon = icon
+    }
+    
+    init(id: String, name: String, icon: String) {
         self.id = id
         self.name = name
+        self.icon = icon
     }
 }
