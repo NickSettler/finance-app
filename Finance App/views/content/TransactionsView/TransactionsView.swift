0d2119d4a612d754d3dbbe4c6d9445873504c3f0
NickSettler <moiseevnikita14@gmail.com>
2023-05-24 03:07:56 +0200

    feat(transaction): add transactions view

---
+++ Finance App/views/content/TransactionsView/TransactionsView.swift
@@ -0,0 +1,149 @@
+//
+//  TransactionsView.swift
+//  Finance App
+//
+//  Created by Никита Моисеев on 24.05.2023.
+//
+
+import SwiftUI
+
+struct TransactionsView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @Environment(\.colorScheme) var colorScheme
+    
+    @StateObject private var viewModel = TransactionsViewViewModel()
+    
+    var noTransactions: some View {
+        VStack(alignment: .center) {
+            Text("No transactions found")
+                .frame(maxWidth: .infinity)
+                .font(.title2)
+                .foregroundColor(.gray)
+            
+            Spacer()
+        }
+    }
+    
+    var filterSheet: some View {
+        NavigationView {
+            VStack(spacing: 12) {
+                DatePicker(
+                    selection: $viewModel.editingAfterDate,
+                    in: ...Date.now,
+                    displayedComponents: .date
+                ) {
+                    Text("After: ")
+                }
+                .onChange(of: viewModel.editingAfterDate) { value in
+                    viewModel.editingBeforeDate = max(
+                        value,
+                        viewModel.editingBeforeDate
+                    )
+                }
+                
+                DatePicker(
+                    selection: $viewModel.editingBeforeDate,
+                    in: ...Date.now,
+                    displayedComponents: .date
+                ) {
+                    Text("Before: ")
+                }
+                .onChange(of: viewModel.editingBeforeDate) { value in
+                    viewModel.editingAfterDate = min(
+                        value,
+                        viewModel.editingAfterDate
+                    )
+                }
+                
+                Spacer()
+            }
+            .padding(16)
+            .navigationBarItems(
+                leading: Button {
+                    viewModel.isFilterSheetPresent = false
+                } label: {
+                    Text("Close")
+                },
+                trailing: Button {
+                    viewModel.applyFilters()
+                } label: {
+                    Text("Apply")
+                }
+            )
+        }
+    }
+    
+    var transactionsList: some View {
+        List(viewModel.filteredTransactions, id: \.id) { transaction in
+            HStack(alignment: .center, spacing: 20) {
+                let isExpense = transaction.amount < 0
+                
+                Image(systemName: isExpense ? "arrow.down.circle.fill" : "arrow.up.circle.fill")
+                    .foregroundColor(isExpense ? .red : .green)
+                
+                VStack(alignment: .leading, spacing: 4) {
+                    HStack(alignment: .top, spacing: 4) {
+                        Text("\(transaction.name ?? "")")
+                        Spacer()
+                        Text(transaction.timestamp.dateValue(), style: .offset)
+                            .font(.caption2)
+                    }
+                    
+                    Text("\(transaction.amount.formatted(.currency(code: "CZK")))")
+                        .font(.caption)
+                }
+            }
+        }
+        .listStyle(.grouped)
+        .background(colorScheme == .light ? .white : .black)
+        .scrollContentBackground(.hidden)
+    }
+    
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 12) {
+            HStack(alignment: .center) {
+                Spacer()
+                
+                Button {
+                    viewModel.displayFilters()
+                } label: {
+                    HStack(alignment: .center, spacing: 4) {
+                        Image(systemName: "line.3.horizontal.decrease")
+                        
+                        Text("Filter")
+                    }
+                }
+                .buttonStyle(.borderedProminent)
+            }
+            .padding(12)
+            .sheet(isPresented: $viewModel.isFilterSheetPresent) {
+                filterSheet
+            }
+            
+            if viewModel.filteredTransactions.count == 0 {
+                List {
+                    noTransactions
+                        .listRowSeparatorTint(.clear)
+                        .listRowBackground(Color.clear)
+                        .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                }
+                .listStyle(.plain)
+            } else {
+                transactionsList
+            }
+        }
+        .onAppear {
+            viewModel.fetchTransactions()
+        }
+        .refreshable {
+            viewModel.fetchTransactions()
+        }
+    }
+}
+
+struct TransactionsView_Previews: PreviewProvider {
+    static var previews: some View {
+        TransactionsView()
+    }
+}
---
+++ Finance App/views/content/TransactionsView/TransactionsViewViewModel.swift
@@ -0,0 +1,64 @@
+//
+//  TransactionsViewViewModel.swift
+//  Finance App
+//
+//  Created by Никита Моисеев on 24.05.2023.
+//
+
+import Foundation
+import FirebaseAuth
+
+@MainActor class TransactionsViewViewModel : ObservableObject {
+    private(set) var currentUser: User?
+    
+    @Published var transactions: [Transaction] = [
+//        .init(id: "124", name: "Salary", amount: 12, timestamp: .init(date: .now.addingTimeInterval(TimeInterval(-60 * 60 * 24 * 7)))),
+//        .init(id: "123", name: "Food", amount: -100, timestamp: .init(date: .now.addingTimeInterval(TimeInterval(-60 * 60 * 24 * 13))))
+    ]
+    @Published var isFilterSheetPresent: Bool = false
+    
+    @Published var afterDate: Date?
+    @Published var editingAfterDate: Date = .now
+    @Published var beforeDate: Date?
+    @Published var editingBeforeDate: Date = .now
+    
+    var filteredTransactions: [Transaction] {
+        self.transactions
+            .filter { transaction in
+                let isAfterDateMatch = (afterDate ?? transaction.timestamp.dateValue()) <= transaction.timestamp.dateValue()
+                let isBeforeDateMatch = (beforeDate ?? transaction.timestamp.dateValue()) >= transaction.timestamp.dateValue()
+                
+                return isAfterDateMatch && isBeforeDateMatch
+            }
+    }
+    
+    init() {
+        self.currentUser = Auth.auth().currentUser
+    }
+    
+    func fetchTransactions() {
+        Task {
+            guard let uid = self.currentUser?.uid else { return }
+            
+            do {
+                self.transactions = try await TransactionService.getTransactions(for: uid).get()
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    func displayFilters() {
+        editingAfterDate = afterDate ?? .now
+        editingBeforeDate = beforeDate ?? .now
+        
+        isFilterSheetPresent = true
+    }
+    
+    func applyFilters() {
+        afterDate = editingAfterDate
+        beforeDate = editingBeforeDate
+        
+        isFilterSheetPresent = false
+    }
+}
