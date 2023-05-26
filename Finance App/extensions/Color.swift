9c5f8295eeb3301b1623fe383076522a1543923c
NickSettler <moiseevnikita14@gmail.com>
2023-05-27 00:25:10 +0200

    feat(theme): add theming

---
+++ Finance App/extensions/Color.swift
@@ -0,0 +1,39 @@
+//
+//  Color.swift
+//  Finance App
+//
+//  Created by Никита Моисеев on 26.05.2023.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static var theme: Color  {
+        return Color("theme")
+    }
+    
+    static var BackgroundColor: Color  {
+        return Color("BackgroundColor")
+    }
+    
+    static var BackgroundColorList: Color  {
+        return Color("BackgroundColorList")
+    }
+    
+    static var ColorPrimary: Color  {
+        return Color("ColorPrimary")
+    }
+    
+    static var Accent: Color  {
+        return Color("AccentColor")
+    }
+    
+    static var TextColorPrimary: Color  {
+        return Color("TextColorPrimary")
+    }
+    
+    static var TextColorSecondary: Color  {
+        return Color("TextColorSecondary")
+    }
+}
