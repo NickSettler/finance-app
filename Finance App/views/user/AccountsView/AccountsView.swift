20442bd2ab9090b4d0a3cffb8e4d9ae803bb3e25
NickSettler <moiseevnikita14@gmail.com>
2023-05-27 15:09:32 +0200

    refactor: merge view and view models

--- Finance App/views/user/AccountsView/AccountsView.swift
+++ Finance App/views/user/AccountsView/AccountsView.swift
@@ -1,20 +1,20 @@
 //
-//  UserEdit.swift
+//  AccountsView.swift
 //  Finance App
 //
-//  Created by Никита Моисеев on 24.03.2023.
+//  Created by Никита Моисеев on 26.05.2023.
 //
 
 import SwiftUI
 
-struct UserEditView: View {
+struct AccountsView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct UserEdit_Previews: PreviewProvider {
+struct AccountsView_Previews: PreviewProvider {
     static var previews: some View {
-        UserEditView()
+        AccountsView()
     }
 }
---
+++ Finance App/views/user/AccountsView/AccountsViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  AccountsViewModel.swift
+//  Finance App
+//
+//  Created by Никита Моисеев on 26.05.2023.
+//
+
+import Foundation
+
+@MainActor class AccountsViewModel : ObservableObject {
+    init() {
+        
+    }
+}
