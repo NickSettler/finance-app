d42a6fdd9a221dfb0df46d17fd2d43192e68d35f
NickSettler <moiseevnikita14@gmail.com>
2023-05-28 00:50:18 +0200

    feat(extension): add binding nil-coalescing operator

---
+++ Finance App/extensions/Binding.swift
@@ -0,0 +1,16 @@
+//
+//  Binding.swift
+//  Finance App
+//
+//  Created by Никита Моисеев on 28.05.2023.
+//
+
+import Foundation
+import SwiftUI
+
+func ??<T>(lhs: Binding<Optional<T>>, rhs: T) -> Binding<T> {
+    Binding(
+        get: { lhs.wrappedValue ?? rhs },
+        set: { lhs.wrappedValue = $0 }
+    )
+}
