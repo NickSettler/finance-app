b9235cf3299de9ae8775ada1ccf869761ca4e0c0
NickSettler <moiseevnikita14@gmail.com>
2023-05-27 14:56:02 +0200

    feat(styles): add new text field style

---
+++ Finance App/styles/RoundedTextFieldStyle.swift
@@ -0,0 +1,25 @@
+//
+//  RoundedTextFieldStyle.swift
+//  Finance App
+//
+//  Created by Никита Моисеев on 27.05.2023.
+//
+
+import SwiftUI
+
+struct RoundedTextFieldStyle : TextFieldStyle {
+    @FocusState var isFocused: Bool
+    
+    func _body(configuration: TextField<Self._Label>) -> some View {
+        configuration
+                .focused($isFocused)
+                .padding(.vertical, 12)
+                .padding(.horizontal, 16)
+                .foregroundColor(.TextColorPrimary)
+                .background(
+                    RoundedRectangle(cornerRadius: 10, style: .continuous)
+                        .strokeBorder(isFocused ? Color.Accent : Color.TextColorSecondary, lineWidth: 2)
+                )
+                .animation(isFocused ? .easeIn(duration: 0.1) : .easeOut(duration: 0.0), value: isFocused)
+    }
+}
