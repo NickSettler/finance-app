a9d675853fa52217e468d0e21293f20d14f6a9af
NickSettler <moiseevnikita14@gmail.com>
2023-05-28 03:51:50 +0200

    fix(category): change categories chart rows layout

--- Finance App/components/PieChart/PieChartRows.swift
+++ Finance App/components/PieChart/PieChartRows.swift
@@ -18,25 +18,28 @@ struct PieChartRows: View {
         
         VStack(spacing: 0) {
             ForEach(0..<self.values.count, id: \.self) { i in
-                HStack {
+                HStack(alignment: .top) {
                     RoundedRectangle(cornerRadius: 5.0)
                         .fill(self.colors[i])
                         .frame(width: 20, height: 20)
                     
                     Text(self.names[i])
+                        .font(.headline)
                         .foregroundColor(.TextColorPrimary)
                     
                     Spacer()
                     VStack(alignment: .trailing) {
                         Text(self.values[i], format: .currency(code: "CZK"))
+                            .font(.callout)
                             .foregroundColor(.TextColorPrimary)
                         
                         Text(String(format: "%.0f%%", self.values[i] / total * 100))
+                            .font(.caption)
                             .foregroundColor(.TextColorSecondary)
                     }
                 }
                 .padding(.vertical, 8)
-                .background(Color.BackgroundColor)
+                .background(Color.ColorPrimary)
                 .onTapGesture {
                     withAnimation(.easeInOut(duration: 0.15)) {
                         if selectedIndex == i {
