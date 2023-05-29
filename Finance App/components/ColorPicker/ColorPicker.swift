e7a108bd7ed209b52f5be55264714cdb80164d71
NickSettler <moiseevnikita14@gmail.com>
2023-05-30 00:39:12 +0200

    feat(color): add color picker component

---
+++ Finance App/components/ColorPicker/ColorPicker.swift
@@ -0,0 +1,63 @@
+//
+//  ColorPicker.swift
+//  Finance App
+//
+//  Created by Никита Моисеев on 29.05.2023.
+//
+
+import SwiftUI
+
+struct ColorPicker: View {
+    @Environment(\.presentationMode) var presentationMode
+    
+    var colors: [Color]
+    @Binding var color: Color
+    
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false) {
+            LazyVGrid(
+                columns: [GridItem(.adaptive(minimum: 64, maximum: 64))]
+            ) {
+                ForEach(colors, id: \.self) { color in
+                    Button {
+                        self.color = color
+                        presentationMode.wrappedValue.dismiss()
+                    } label: {
+                        RoundedRectangle(cornerRadius: 4)
+                            .fill(color)
+                            .padding(20)
+                            .frame(maxWidth: .infinity, minHeight: 64)
+                            .background(Color(UIColor.systemGroupedBackground))
+                            .cornerRadius(8)
+                            .foregroundColor(.primary)
+                    }
+                    .buttonStyle(.plain)
+                    .hoverEffect(.lift)
+                }
+            }
+            .padding()
+            .toolbar {
+                ToolbarItemGroup(placement: .cancellationAction) {
+                    Button {
+                        presentationMode.wrappedValue.dismiss()
+                    } label: {
+                        Text("Cancel")
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ColorPicker_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorPicker(
+            colors: categoryColors,
+            color: .init(get: {
+                return .black
+            }, set: { color in
+                print(String(format: "%x", color.toHex() ?? 0))
+            })
+        )
+    }
+}
